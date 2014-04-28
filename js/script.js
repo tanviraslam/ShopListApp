a28e5275c953f79896eb84ff16ab8db450d96854
Tanvir Aslam <tanviraslam@outlook.com>
2014-04-28 02:36:57 -0700

    Finshed shopping list app without much styling

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+	$(".additem,.vplus,.hplus").hover(function(){
+		$('.additem').css("background", "#27AE60");
+		$('.vplus,.hplus').css("background", "#fff");
+	}, function(){
+		$('.additem').css("background", "#fff");
+		$('.vplus,.hplus').css("background", "#2ECC71");
+	});
+
+	//Adding item. Check for when the input is empty
+	$('.additem').click(function(){
+		var $iteminput = $('#iteminput');
+		var itemHtml = '<div class="listitem"><div class="content">'+$iteminput.val()+'</div><i class="fa fa-check tick fa-2x"></i><i class="fa fa-times cross fa-2x"></i></div>'
+		$('#results').append(itemHtml);
+		$iteminput.val("");
+	});
+
+	//Selecting item in result list
+	$(document.body).on('click','.content',function(e){
+		e.stopPropagation();
+		e.preventDefault();
+		var $listitem = $(this).parent();
+		$listitem.toggleClass('itemselected');
+		var $icons = $listitem.children('.tick, .cross');
+		$(this).toggleClass('contentwidth');
+		if($listitem.hasClass('itemselected')){
+			$icons.css({
+				"display" : "inline-block"
+			});
+		}else{
+			$icons.css("display","none");
+		}
+	});
+
+	//hover, select, unselect tick and cross icons
+	$(document.body).on('click','.tick',function(){
+		$(this).css("color","#fff");
+		$(this).next().css("color","#fff");
+		$(this).parent().addClass('taskdone').children('.content').css("text-decoration","line-through");
+	});
+
+	$(document.body).on('click','.cross',function(){
+		$(this).css("color","red");
+		$(this).parent().remove();
+	});
+});
