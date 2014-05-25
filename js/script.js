bcc67647cddc983b04af9244805af53e70e80a68
Tanvir Aslam <mtanvir.upenn@gmail.com>
2014-05-25 01:16:52 -0700

    Polished it further. Added list items as notes to allow for future additions like categories, deadlines etc.

--- js/script.js
+++ js/script.js
@@ -19,16 +19,16 @@ $(document).ready(function(){
 			event.stopPropagation();
 			addItem();
 		}
-	});;
+	});
 
 	//Selecting item in result list
-	$(document.body).on('click','.content',function(e){
+	$(document.body).on('mouseenter','.listitem',function(e){
 		e.stopPropagation();
 		e.preventDefault();
-		var $listitem = $(this).parent();
-		$listitem.toggleClass('itemselected');
+		var $listitem = $(this);
+		$listitem.addClass('itemselected');
 		var $icons = $listitem.children('.tick, .cross');
-		$(this).toggleClass('contentwidth');
+		// $(this).toggleClass('contentwidth');
 		if($listitem.hasClass('itemselected')){
 			$icons.css({
 				"display" : "inline-block"
@@ -36,15 +36,24 @@ $(document).ready(function(){
 		}else{
 			$icons.css("display","none");
 		}
+	}).on('mouseleave','.listitem',function(e){
+		var $listitem = $(this);
+		var $icons = $listitem.children('.tick, .cross');
+		$listitem.removeClass('itemselected');
+		$icons.css("display","none");
 	});
 
 	//hover, select, unselect tick and cross icons
 	$(document.body).on('click','.tick',function(){
-		$(this).css("color","#fff");
-		$(this).next().css("color","#fff");
 		var $parent = $(this).parent();
-		$parent.addClass('taskdone').children('.content').css("text-decoration","line-through");
-		
+		if($(this).hasClass('ticked')){
+			$(this).removeClass('ticked').css('color','red').next().css('color','green');
+			$parent.removeClass('taskdone').children('.content').css("text-decoration", 'none');
+		}else{
+			$(this).css("color","#fff").addClass('ticked');
+			$(this).next().css("color","#fff");
+			$parent.addClass('taskdone').children('.content').css("text-decoration","line-through");
+		}
 	});
 
 	$(document.body).on('click','.cross',function(){
@@ -54,16 +63,41 @@ $(document).ready(function(){
 			$(this).remove();
 		});
 	});
+
+	//Slider input form for more detailed list entry
+	// $('#iteminput').click(function(){
+	// 	$('#inputoptions').slideToggle();
+	// });
+
+	//Checking screen size, to adjust the tick and cross icons
+	
+	//Making the item content editable
+	$(document.body)
+		.on('mouseover', '.content', function(){
+			$(this).css('cursor', 'text');
+		})
+		.on('click', '.content', function(){
+			$(this).attr('contenteditable',function(index, value){
+				console.log("test");
+				if(value.toLowerCase() === 'true') return false;
+				else return true;
+			});
+		});
 });
 
 
 function addItem(){
 	var $iteminput = $('#iteminput');
+	var $colors = ['#1ABC9C', '#2ECC71', '#3498DB', '#E67E22', '#E74C3C', '#F39C12', '#D35400'];
+	
 	if($iteminput.val()){
-		var itemHtml = '<div class="listitem"><div class="content">'+$iteminput.val()+'</div><i class="fa fa-check tick fa-2x"></i><i class="fa fa-times cross fa-2x"></i></div>';
+		var $randCol = $colors[Math.floor(Math.random() * $colors.length)];
+		console.log($randCol);
+		var itemHtml = '<div class="listitem" style="background:' +$randCol +';"><div class="content" contenteditable=false>'+$iteminput.val()+'</div><i class="fa fa-check tick fa-2x"></i><i class="fa fa-times cross fa-2x"></i></div>';
 		$('#results').append(itemHtml);
 		$iteminput.val("");
 	}else{
 		alertify.alert('Please enter an item');
 	}
 }
+
